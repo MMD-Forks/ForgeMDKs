d8956ccfb22551551ea0ae92723d12a14139aefa
Jasmine Iwanek <jasmine@iwanek.co.uk>
2021-07-07 03:44:32 -0400

    Update ExampleMod.java for mapping differences

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -15,6 +15,6 @@ public class ExampleMod
     public void init(FMLInitializationEvent event)
     {
 		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
     }
 }
