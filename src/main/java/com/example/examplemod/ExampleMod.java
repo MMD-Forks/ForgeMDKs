e5a47fa874ff6c5c69f92a4e8b89e33089cb768c
Jasmine Iwanek <jasmine@iwanek.co.uk>
2021-07-09 02:49:25 -0400

    Initial 1.9.4

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -14,7 +14,7 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
+        // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
     }
 }
