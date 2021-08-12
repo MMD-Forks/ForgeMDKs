0f4c4a842c232d0cdb4cd4c21c966afef270499f
Jasmine Iwanek <jasmine@iwanek.co.uk>
2021-08-12 00:37:19 -0400

    Minor Updates

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,20 +1,33 @@
 package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
+
+import org.apache.logging.log4j.Logger;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
+    public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
-    
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.dirt.getUnlocalizedName());
     }
 }
